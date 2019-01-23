f5153d44db839e2f6d8f6977203d1f1ccbb4d474
Jared <razrsword@gmail.com>
2019-01-23 13:00:29 -0500

    Errors pring, can't find file

--- grails-app/services/streama/FileService.groovy
+++ grails-app/services/streama/FileService.groovy
@@ -19,10 +19,10 @@ class FileService {
 
     def fileLength = rawFile.length()
     def contentLength = rawFile.length().toString()
-    def rangeEnd = fileLength.toLong()-1
+    def rangeEnd = fileLength.toLong() - 1
     def rangeStart
 
-    if(rangeHeader){
+    if (rangeHeader) {
       String[] range = rangeHeader.substring(6).split("-")
       rangeStart = range[0].toLong()
       if (range.length == 2)
@@ -37,79 +37,101 @@ class FileService {
     response.addHeader("Cache-Control", 'public,max-age=3600,public')
     response.addHeader("Etag", file.sha256Hex)
 
+    System.out.println("Headers Added")
 
-    if(rangeHeader){
+
+    if (rangeHeader) {
       response.addHeader("Content-Range", "bytes $rangeStart-$rangeEnd/$fileLength")
       response.setStatus(PARTIAL_CONTENT.value())
     }
 
+    System.out.println("Range Header Set")
 
-    FileInputStream fis
+    InputStream fis
 
-    //Read and write bytes of file incrementally into the outputstream
-    try{
-      fis = new FileInputStream(rawFile) //391694394
-    }catch(e){
-      response.setStatus(PRECONDITION_FAILED.value())
-      return [error: true, message: e.message]
-    }
+    System.out.println("Above Process Builder")
+
     byte[] buffer = new byte[16000]
 
-    if(rangeStart){
-      fis.skip(rangeStart)
+    int skip = 0
+    if (rangeStart) {
+      skip = rangeStart
     }
 
-    try{
-      while (true){
+    ProcessBuilder pb = new ProcessBuilder("ffmpeg"
+      , "-skip_initial_bytes", skip.toString()
+      , "-re"
+      , "-i", "\"" + filePath + "\""
+      , "-vcodec", "libx264"
+      , "-preset", "veryfast"
+      , "-crf", "30"
+      , "-acodec", "aac"
+      , "-f", "mpegts"
+      , "-")
+    Process p = pb.start()
+    System.out.println("Process Builder started")
+
+    fis = p.getInputStream()
+
+    System.out.println("Input Stream Grabbed")
+
+    BufferedReader reader =
+      new BufferedReader(new InputStreamReader(p.getErrorStream()))
+    try {
+      while (true) {
+        String line = null
+        while ( (line = reader.readLine()) != null) {
+          System.out.println(line)
+        }
         int read = fis.read(buffer)
-        if (read == -1){
+        if (read == -1) {
           break
         }
         response.outputStream.write(buffer, 0, read)
       }
-    }catch(Exception e){
+    } catch (Exception e) {
 //      log.error('caught exception for video playback. ' + e.message)
 //      e.printStackTrace()
 //      e.getCause().printStackTrace()
-    }finally{
+    } finally {
 //      response.outputStream.flush()
       fis.close()
     }
   }
 
 
-  def Map fullyRemoveFile(File file){
-    if(!file){
+  def Map fullyRemoveFile(File file) {
+    if (!file) {
       return ResultHelper.generateErrorResult(SC_NOT_ACCEPTABLE, 'file', 'No valid file selected.')
     }
-    if(file.localFile){
+    if (file.localFile) {
       return ResultHelper.generateErrorResult(SC_NOT_ACCEPTABLE, 'local', 'cant delete file associated with the File-Browser.')
     }
-    if(file.externalLink){
+    if (file.externalLink) {
       return ResultHelper.generateErrorResult(SC_NOT_ACCEPTABLE, 'external', 'cant delete file associated with an external Link.')
     }
-    if(file.associatedVideosInclDeleted){
-      file.associatedVideosInclDeleted.each{ video ->
+    if (file.associatedVideosInclDeleted) {
+      file.associatedVideosInclDeleted.each { video ->
         video.removeFromFiles(file)
         video.save(flush: true, failOnError: true)
       }
     }
 
-    if(file.isInUse){
+    if (file.isInUse) {
       def tvShowByPoster = TvShow.findByPoster_image(file)
-      if(tvShowByPoster){
+      if (tvShowByPoster) {
         tvShowByPoster.poster_image = null
         tvShowByPoster.save(flush: true, failOnError: true)
       }
 
       def tvShowByBackdrop = TvShow.findByPoster_image(file)
-      if(tvShowByBackdrop){
+      if (tvShowByBackdrop) {
         tvShowByBackdrop.backdrop_image = null
         tvShowByBackdrop.save(flush: true, failOnError: true)
       }
     }
 
-    if(file.imagePath && file.fileExists){
+    if (file.imagePath && file.fileExists) {
       java.io.File rawFile = new java.io.File(file.imagePath)
       rawFile.delete()
     }
